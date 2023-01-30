aa3e400e0e930d24421feff682bd16445269d245
flischewski1 <thechocogamer@yahoo.de>
2023-01-30 14:25:23 +0100

    trying things

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -125,3 +125,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SCM_DO_BUILD_DURING_DEPLOYMENT = 1
